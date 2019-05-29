90c7554066df96116a1df76eb6862209e79546d5
Camilia <36171540+camiliamzali@users.noreply.github.com>
2019-05-29 12:06:44 -0400

    Add files via upload

---
+++ client/build/precache-manifest.848cf44701c4a653146d2266c3dccbdc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae615f4de61c4c6d457a793e5aedf60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3864d6bf08df5d6ae79b",
+    "url": "/static/js/2.ce2c4187.chunk.js"
+  },
+  {
+    "revision": "ba14032b668509c0376e",
+    "url": "/static/js/main.ac72cd79.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
